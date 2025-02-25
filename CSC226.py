df4edabe587f112262f141aee9f63b8a4fc7e853
gracecostello26 <grace.costello@cix.csi.cuny.edu>
2025-02-25 18:04:10 -0500

    Add files via upload
    CSC226 HW#1

---
+++ CSC226.py
@@ -0,0 +1,28 @@
+#Notes
+ 
+#print hello world
+print("Hello World!")
+#define a function that prints hello and the users name
+def greet(name):
+    print("Hello " + name + "!")
+
+greet("Grace")
+
+#Define a variable
+x = 5
+#display the variable
+print(x)
+#display the type of the variable
+print(type(x))
+
+#create a loop
+for i in range(x):
+    print(i)
+
+#Create an array
+names = ["Grace", "Ada", "Alan"]
+
+#loop through the array
+for name in names:
+    greet(name)
+
